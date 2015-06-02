d7510aa4ddf53609f617a65b381e460a38d9d1ad
ayezutov <alexander.yezutov@gmail.com>
2015-06-01 23:53:40 -0400

    Adding another file missing from Git

---
+++ LingSubPlayer.DataAccess/RepositorySettings.cs
@@ -0,0 +1,12 @@
+﻿namespace LingSubPlayer.DataAccess
+{
+    public class RepositorySettings : IRepositorySettings
+    {
+        public RepositorySettings(string connectionData)
+        {
+            ConnectionData = connectionData;
+        }
+
+        public string ConnectionData { get; private set; }
+    }
+}
